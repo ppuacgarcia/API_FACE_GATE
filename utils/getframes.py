c14647c774761e0e27bb89c9970bd4ffac456007
ppuacgarcia <pablopuacg@gmail.com>
2024-04-19 03:30:11 -0600

    fixed get frames

--- utils/getframes.py
+++ utils/getframes.py
@@ -8,7 +8,7 @@ class MakeFrames:
         person_path = data_path + '/' + person_name
         
         if not os.path.exists(person_path):
-            print('Carpeta creada: ',person_path)
+            print('Carpeta para frames creada: ',person_path)
             os.makedirs(person_path)
 
         #cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
@@ -16,26 +16,26 @@ class MakeFrames:
 
         faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
         count = 0
-        if not cap:
-            print('Video cargado correctamente')
+    
         while True:
             ret, frame = cap.read()
             if ret == False: break
             frame =  imutils.resize(frame, width=640)
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             auxFrame = frame.copy()
-
+            
             faces = faceClassif.detectMultiScale(gray,1.3,5)
 
             for (x,y,w,h) in faces:
                 cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
                 rostro = auxFrame[y:y+h,x:x+w]
                 rostro = cv2.resize(rostro,(150,150),interpolation=cv2.INTER_CUBIC)
-                cv2.imwrite(person_path + '/rostro_{}.jpg'.format(count),rostro)
+                cv2.imwrite(person_path + '/rotro_{}.jpg'.format(count),rostro)
                 count = count + 1
+            cv2.imshow('frame',frame)
 
             k =  cv2.waitKey(1)
-            if k == 27 or count >= 1000:
+            if k == 27 or count >= 300:
                 break
 
         cap.release()
